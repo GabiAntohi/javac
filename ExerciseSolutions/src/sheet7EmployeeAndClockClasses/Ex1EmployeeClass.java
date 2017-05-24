4cbd05b412e5a14d732e6856c44e86d2109601a3
GabiAntohi <gabrielaantohi.eu@gmail.com>
2017-05-24 10:49:48 +0100

    change exercise

--- ExerciseSolutions/src/sheet7EmployeeAndClockClasses/Ex1EmployeeClass.java
+++ ExerciseSolutions/src/sheet7EmployeeAndClockClasses/Ex1EmployeeClass.java
@@ -1,92 +1,84 @@
 package sheet7EmployeeAndClockClasses;
 
 public class Ex1EmployeeClass {
-private String name;
-private int age;
-private double salary;
-private int noSickDays;
-private int employeeNumber;
-
-public static int employeeCounter;//counter
-public Ex1EmployeeClass() {//constructors
-	employeeCounter++;
-	salary=32000;//default
-} 
-public Ex1EmployeeClass(String name){
-	setName(name);	
-}
-public Ex1EmployeeClass(int age, int noSickDays, int employeeNumber){
-	setAge(age);
-	setNoSickDays(noSickDays);
-	setEmployeeNumber(employeeNumber);
-	
-}
-
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-public int getAge() {
-	return age;
-}
-public void setAge(int age) {
-	this.age = age;
-}
-public double getSalary() {
-	return salary;
-}
-public void calculateSalary(double salary, int age, int noSickDays) {
-	salary=32000;
-	if  (age>=25&&age<=30)salary=(salary+salary*2/100);
-	else if 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	this.salary = salary;
-}
-public int getNoSickDays() {
-	return noSickDays;
-}
-public void setNoSickDays(int noSickDays) {
-	this.noSickDays = noSickDays;
-}
-public int getEmployeeNumber() {
-	return employeeNumber;
-}
-public void setEmployeeNumber(int employeeNumber) {
-	this.employeeNumber = employeeNumber;
-}
-
-public double increaseSalary(){
-	double increasedSalary=(salary+(salary*8/100));	
-	return increasedSalary;
-}
+	private String name;
+	private int age;
+	private double salary;
+	private int noSickDays;
+	private int employeeNumber;
 
 
-@Override
-public String toString() {
-	return "name of employee=" + name + 
-			"\nage=" + age +
-			"\nsalary=" + salary +
-			"\nnoSickDays=" + noSickDays +
-			"\nemployeeNumber=" + employeeNumber +
-			"\n getName()=" + getName() + 
-			"\n getAge()=" + getAge()
-			+ ", getSalary()=" + getSalary() +
-			"\ngetNoSickDays()=" + getNoSickDays() + 
-			"\n getEmployeeNumber()="+ getEmployeeNumber() + 
-			"\n increaseSalary()=" + increaseSalary();
-}
+	public static int employeeCounter = 10;//counter
+	public Ex1EmployeeClass() {//constructors
+		salary=32000;
+		age=27;
+		noSickDays=0;
+		//default
+		employeeCounter++;
+	} 
 
+	public Ex1EmployeeClass(String name, int age, int noSickDays){
+		this();
+		setName(name);
+		setAge(age);
+		setNoSickDays(noSickDays);
 	
+	}
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
 	
+
+	public double calculateSalary() {
+		salary=32000;
+		if(age>=18&&age<25)salary=32000;
+		else if  (age>=25&&age<=30)salary=(salary+salary*7/100);
+		else if (age>30&&age<=40)salary=(salary+salary*15/100);
+		else if (age>40&&age<=67)salary=(salary+salary*35/100);
+		else {salary=salary-2000;}
+		salary=salary-(noSickDays*(salary/260));
+		return salary;
 	
+	}
+	public int getNoSickDays() {
+		return noSickDays;
+	}
+	public void setNoSickDays(int noSickDays) {
+		this.noSickDays = noSickDays;
+	}
+	public int getEmployeeNumber() {
+		return employeeNumber;
+	}
+	public void setEmployeeNumber(int employeeNumber) {
+		this.employeeNumber = employeeNumber;
+	}
+
+	public double increaseSalary(){
+		double increasedSalary=(salary+(salary*8/100));	
+		return increasedSalary;
+	}
+
+	@Override
+	public String toString() {
+		return "Name of employee: " + name + 
+				"\nAge: " + age +
+				String.format("\nSalary:%.2f ",  calculateSalary()) + 
+				"\nSick Days: " + noSickDays +
+				String.format("\nIncreased Salary:%.2f  ", increaseSalary()) +
+				"\nHow many employees: " + employeeCounter + "\n";
+	}
+
+
+
+
 }
--- ExerciseSolutions/src/sheet7EmployeeAndClockClasses/MainForEx1EmployeeClass.java
+++ ExerciseSolutions/src/sheet7EmployeeAndClockClasses/MainForEx1EmployeeClass.java
@@ -5,17 +5,20 @@ package sheet7EmployeeAndClockClasses;
 public class MainForEx1EmployeeClass {
 
 	public static void main(String[] args) {
-		Ex1EmployeeClass em1 = new Ex1EmployeeClass(23, 5, 2323);
+		Ex1EmployeeClass em1 = new Ex1EmployeeClass("Mark", 27, 5);
 		System.out.println(em1);  
 		
 		
 		
-		Ex1EmployeeClass em2 = new Ex1EmployeeClass(27, 7, 2323);
+		Ex1EmployeeClass em2 = new Ex1EmployeeClass("John",50, 7);
 		System.out.println(em2);  
 		
 		
-		Ex1EmployeeClass em3 = new Ex1EmployeeClass(29, 20, 2323);
+		Ex1EmployeeClass em3 = new Ex1EmployeeClass("Eddy",37, 20);
 		System.out.println(em3); 
+		
+		Ex1EmployeeClass em4 = new Ex1EmployeeClass("June",18, 0);
+		System.out.println(em4); 
 
 	}
 
