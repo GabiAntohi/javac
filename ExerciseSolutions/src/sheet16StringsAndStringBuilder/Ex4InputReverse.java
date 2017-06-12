6f37fadc94c2bef6be1bcbd6438af360090a8068
GabiAntohi <gabrielaantohi.eu@gmail.com>
2017-06-12 11:27:42 +0100

    commit

--- ExerciseSolutions/src/sheet16StringsAndStringBuilder/Ex4InputReverse.java
+++ ExerciseSolutions/src/sheet16StringsAndStringBuilder/Ex4InputReverse.java
@@ -1,9 +1,23 @@
 package sheet16StringsAndStringBuilder;
-
+import java.util.Scanner;
 public class Ex4InputReverse {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		Scanner s = new Scanner (System.in);
+		System.out.println("Input a String");
+		String input = s.nextLine();
+		
+		StringBuilder sb=new StringBuilder(input);  
+		sb.reverse();  
+		System.out.println(sb);
+		
+	    char[] letters = input.toCharArray();
+	    for(int counter=letters.length - 1; counter >= 0;counter--)
+	    System.out.print(letters[counter]);
+	    
+	    s.close();
 
 	}
 
