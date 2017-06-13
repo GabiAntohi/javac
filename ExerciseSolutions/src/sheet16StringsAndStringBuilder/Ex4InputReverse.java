c259e551a499178aed9670b46281a4283dde7092
GabiAntohi <gabrielaantohi.eu@gmail.com>
2017-06-13 10:48:39 +0100

    commit

--- ExerciseSolutions/src/sheet16StringsAndStringBuilder/Ex4InputReverse.java
+++ ExerciseSolutions/src/sheet16StringsAndStringBuilder/Ex4InputReverse.java
@@ -3,20 +3,19 @@ import java.util.Scanner;
 public class Ex4InputReverse {
 
 	public static void main(String[] args) {
-		
-		
+
 		Scanner s = new Scanner (System.in);
 		System.out.println("Input a String");
 		String input = s.nextLine();
 		
 		StringBuilder sb=new StringBuilder(input);  
 		sb.reverse();  
-		System.out.println(sb);
-		
+		System.out.println("What you typed reversed: " + sb );
+		System.out.println("What you types in reverse:");
 	    char[] letters = input.toCharArray();
 	    for(int counter=letters.length - 1; counter >= 0;counter--)
 	    System.out.print(letters[counter]);
-	    
+
 	    s.close();
 
 	}
---
+++ ExerciseSolutions/src/sheet16StringsAndStringBuilder/Ex5LetterSearch.java
@@ -0,0 +1,47 @@
+package sheet16StringsAndStringBuilder;
+import java.util.Scanner;
+public class Ex5LetterSearch {
+
+	public static void main(String[] args) {
+	//first way	but using the second as well
+		
+		
+	/*	Scanner s = new Scanner (System.in);
+		for(int a=0; a<3; a++){
+		System.out.println("Enter a word");
+		String input = s.nextLine();
+		System.out.println("Enter a search character");
+		String input2 = s.nextLine();
+		System.out.println("The letter you wrote is: " + input2);
+		int position = input.indexOf(input2);
+		char[] letters = input.toCharArray();
+	    for(int i = 0; i<1; i++)
+	    	if(position==(input.indexOf(input2))&&position>=0)
+		//  System.out.print(letters[i]);
+		System.out.println("The letter " + letters[i] + " is at position " + position);
+	    	else System.out.println(input2 + " was not found");
+		}
+		
+	    s.close();*/
+	//second way?	
+		Scanner s = new Scanner (System.in);
+		for(int a=0; a<3; a++){
+		System.out.println("Enter a word");
+		String input = s.nextLine();
+		System.out.println("Enter a search character");
+		String input2 = s.nextLine();
+		System.out.println("The letter you wrote is: " + input2);
+	   
+		if (input.indexOf(input2)!=-1)
+	    { int position = input.indexOf(input2);
+	       System.out.println("there is " + input2 + " in " + input + " and the position is " + position);
+	    }
+	    else
+	    {
+	       System.out.println("there is no " + input2 + " in " + input);
+	    }
+	    }
+	    s.close();
+	}
+
+}
