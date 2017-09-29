a69e320ca9fe28ad67fc6c861bd67ce7dbcc3186
GabiAntohi <gabrielaantohi.eu@gmail.com>
2017-09-29 11:44:08 +0100

    commit exceptions

---
+++ OCA_Revision/src/random/SM.java
@@ -0,0 +1,21 @@
+package random;
+
+public class SM {
+
+public static void main(String [] args) throws Exception{
+	String s = null;
+	int b;
+	try{
+	b = s.length();
+	System.out.println(b);} 
+	catch (Throwable e){
+		System.out.println("exception caught");
+	}
+
+	System.out.println("bye");
+	
+	
+	
+}
+
+}
